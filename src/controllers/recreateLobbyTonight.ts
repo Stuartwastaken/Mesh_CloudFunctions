c7617e6541b69d790f591babf2d137f5fbdc1d32
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-04-03 10:08:03 -0500

    recreateLobbyTonight finished

--- src/controllers/recreateLobbyTonight.ts
+++ src/controllers/recreateLobbyTonight.ts
@@ -4,17 +4,28 @@ import * as admin from "firebase-admin";
 export const recreateLobbyTonight = functions.pubsub
     .schedule("every day 00:00")
     .onRun(async () => {
-      const collectionRef = admin.firestore().collection("lobby_tonight");
+      const lobbyTonightRef = admin.firestore().collection("lobby_tonight");
+      const lobbyTomorrowRef = admin.firestore().collection("lobby_tomorrow");
 
       const batch = admin.firestore().batch();
 
-      const querySnapshot = await collectionRef.get();
-
+      // Delete all documents in the "lobby_tonight" collection
+      const querySnapshot = await lobbyTonightRef.get();
       querySnapshot.forEach((doc) => {
         batch.delete(doc.ref);
       });
 
-      await batch.commit();
+      // Copy all documents from the "lobby_tomorrow"
+      // collection to the "lobby_tonight" collection
+      const querySnapshotTomorrow = await lobbyTomorrowRef.get();
+      querySnapshotTomorrow.forEach((doc) => {
+        const data = doc.data();
+        const newDocRef = lobbyTonightRef.doc(doc.id);
+        batch.set(newDocRef, data);
+      });
 
-      return collectionRef.doc().set({foo: "bar"});
+      await batch.commit();
+      console.log("Successfully recreated the 'lobby_tonight' collection.");
+
+      return null;
     });
